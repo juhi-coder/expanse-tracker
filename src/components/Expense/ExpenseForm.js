a2c7869774ad7d78f8a3bfe350da9061c8a45a4c
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-14 19:26:44 +0530

    task17

--- src/components/Expense/ExpenseForm.js
+++ src/components/Expense/ExpenseForm.js
@@ -3,21 +3,17 @@ import classes from "./ExpenseForm.module.css";
 import ExpenseList from "./ExpenseList";
 import { ExpenseSliceAction } from "../../store/Expense";
 import { useSelector, useDispatch } from "react-redux";
-
 const ExpenseForm = () => {
   const Dispatch = useDispatch();
   const isupdate = useSelector((state) => state.expense.isupdate);
   const temp = useSelector((state) => state.expense.tempitem);
-
-  const enteredAmount = useRef(null);
-  const enteredCategorys = useRef(null);
-  const enteredDescription = useRef(null);
+  const enteredAmount = useRef();
+  const enteredCategorys = useRef();
+  const enteredDescription = useRef();
   let emailId = localStorage.getItem("mailid").replace(/[&@.]/g, "");
   const ExpenseFormHandler = async (event) => {
     event.preventDefault();
     let itemid = localStorage.getItem("itemid");
-
-    // console.log(itemid);
     const ExpenseObject = {
       amount: enteredAmount.current.value,
       categorys: enteredCategorys.current.value,
@@ -42,64 +38,63 @@ const ExpenseForm = () => {
             "Content-Type": "application/json",
           },
         }
-      );
-      const data = await response.json();
-      Dispatch(ExpenseSliceAction.updateList(ExpenseObject));
-      Dispatch(ExpenseSliceAction.setIsupdate(false));
-      enteredAmount.current.value = null;
-      enteredCategorys.current.value = null;
-      enteredDescription.current.value = null;
-    } catch (error) {
-      console.log(error.message);
-    }
-
-  };
-  useEffect(() => {
-
-    const ExpenseFormHandler = async () => {
-      try {
-        const response = await fetch(
-          `https://expance-tracker-2795d-default-rtdb.firebaseio.com/tracker/${emailId}.json`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
         );
         const data = await response.json();
-
-        const transformeddata = [];
-        for (const key in data) {
-          const Obj = {
-            id: key,
-            ...data[key],
-          };
-          transformeddata.push(Obj);
-        }
-        Dispatch(ExpenseSliceAction.AddItem(transformeddata));
+        Dispatch(ExpenseSliceAction.updateList(ExpenseObject));
+        Dispatch(ExpenseSliceAction.setIsupdate(false));
+        enteredAmount.current.value = null;
+        enteredCategorys.current.value = null;
+        enteredDescription.current.value = null;
       } catch (error) {
         console.log(error.message);
       }
     };
-    ExpenseFormHandler();
-  }, [temp]);
-  const onUpdateHandler = (item) => {
 
-    Dispatch(ExpenseSliceAction.setIsupdate(true));
-    enteredAmount.current.value = item.amount;
-    enteredCategorys.current.value = item.categorys;
-    enteredDescription.current.value = item.description;
-    localStorage.setItem("itemid", item.id);
+    useEffect(() => {
+      const ExpenseFormHandler = async () => {
+        try {
+          const response = await fetch(
+            `https://expance-tracker-2795d-default-rtdb.firebaseio.com/tracker/${emailId}.json`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          const data = await response.json();
+          const transformeddata = [];
+          for (const key in data) {
+            const Obj = {
+              id: key,
+              ...data[key],
+            };
+            transformeddata.push(Obj);
+          }
+          Dispatch(ExpenseSliceAction.AddItem(transformeddata));
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+      ExpenseFormHandler();
+    }, [temp]);
+    const onUpdateHandler = (item) => {
+      Dispatch(ExpenseSliceAction.setIsupdate(true));
+      enteredAmount.current.value = item.amount;
+      enteredCategorys.current.value = item.categorys;
+      enteredDescription.current.value = item.description;
+      localStorage.setItem("itemid", item.id);
+    };
 
-  };
-  <div className={classes["ExpenseForm-container"]}>
+    return (
+      <div className={classes["ExpenseForm-container"]}>
       <form className={classes["ExpenseForm"]} onSubmit={ExpenseFormHandler}>
         <h2>ExpenseForm</h2>
         <label htmlFor="amount">Amout</label>
         <input type="text" id="Amout" ref={enteredAmount} required></input>
         <label htmlFor="cars"> category</label>
         <select name="categorys" id="categorys" ref={enteredCategorys}>
+          <option value="None">choose category</option>
           <option value="Food">Food</option>
           <option value="Petrol">Petrol</option>
           <option value="Cloths">Clothes</option>
@@ -107,7 +102,13 @@ const ExpenseForm = () => {
           <option value="etc">Basic Needs</option>
         </select>
         <label htmlFor="description">description</label>
-        <input type="text" id="description" ref={enteredDescription} required></input>
+        <input
+          type="text"
+          id="description"
+          ref={enteredDescription}
+          required
+        ></input>
+
         <button type="submit">{!isupdate ? "submit" : "update"}</button>
       </form>
       <ExpenseList onUpdate={onUpdateHandler}></ExpenseList>
