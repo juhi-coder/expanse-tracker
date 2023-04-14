f20579085074dc0a1f9cbfddb2d3fb80df65e985
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-14 17:21:16 +0530

    task16

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import Store from './components/store/Store';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <BrowserRouter>
     <Provider store={Store}>
-    <App />
+      <App />
     </Provider>
+    </BrowserRouter>
 );
 reportWebVitals();
